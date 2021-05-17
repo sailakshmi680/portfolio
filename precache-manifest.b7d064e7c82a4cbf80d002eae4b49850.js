f45e7004d7f88b4ca9c1c57a6273aeeaa49c6bf5
sailakshmi680 <sailakshmiyyy@gmail.com>
2021-05-17 14:02:41 +0530

    Updates

---
+++ precache-manifest.b7d064e7c82a4cbf80d002eae4b49850.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0669a53f835e2e6b5f3e838aa0e2d55a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c800940a7886e75544d0",
+    "url": "/portfolio/static/css/main.a57cc24f.chunk.css"
+  },
+  {
+    "revision": "962ce89ff0b16b221ff6",
+    "url": "/portfolio/static/js/2.c8956b1e.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/portfolio/static/js/2.c8956b1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c800940a7886e75544d0",
+    "url": "/portfolio/static/js/main.795b6cb5.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "7c8e5f50725885d51b72b45164274e41",
+    "url": "/portfolio/static/media/avatar.7c8e5f50.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/portfolio/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "730958a228d26dee011c5aa0aa3c3db9",
+    "url": "/portfolio/static/media/javascript-fullstack.730958a2.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/portfolio/static/media/react-redux.a8b012a0.jpg"
+  }
+]);
